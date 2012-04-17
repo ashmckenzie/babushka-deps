95db6ea18400e95b44d1153d25997bdbe3906164
Ash McKenzie <ash@greenworm.com.au>
2012-04-17 22:34:47 +1000

    latest

--- app_bundles.rb
+++ app_bundles.rb
@@ -1,6 +1,3 @@
-require 'pry'
-require 'pry-nav'
-
 class AshMcKenzieAppBundles
 
   def initialize
--- ash-macbook-air.rb
+++ ash-macbook-air.rb
@@ -2,4 +2,5 @@ dep 'ash-macbook-air' do
   AshMcKenzieAppBundles.apps.each { |app| requires app.keys.first }
   requires 'Perian.pref_pane'
   requires 'packages'
+  requires 'npm'
 end

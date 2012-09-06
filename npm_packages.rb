008375087b02f17fa47202518a05ce46f76623ee
Ash McKenzie <ash@greenworm.com.au>
2012-09-06 10:27:03 +1000

    improved npm packaging install

--- npm_packages.rb
+++ npm_packages.rb
@@ -1,3 +1,12 @@
 dep 'npm_packages' do
-  system('npm install -g coffee-script')
+  install_npm_package 'coffee-script'
+  install_npm_package 'express'
+end
+
+def install_npm_package package
+  system("npm install -g #{package}") unless npm_package_installed?(package)
+end
+
+def npm_package_installed? package
+  %x{ npm list -g #{package} | grep empty | wc -l }.strip == '0'
 end
